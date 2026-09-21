01af25dae16ac50b3eea16984f77d8ca4415a56d
Srivalli-25 <srivallimutchakarla1306@gmail.com>
2026-09-21 18:40:16 +0530

    Task1

---
+++ src/Selfintro.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+export default function Selfintro() {
+  return (
+    <div className="profile-card">
+      <h1>Self Introduction</h1>
+
+      <h2>Sri Valli Mutchakarla</h2>
+
+      <p><b>Role:</b> Full Stack Developer.</p>
+      <p><b>Education:</b> Bsc Computer Science.</p>
+      <p><b>Skills:</b> C, Java, Python, SQL, MERN</p>
+      <p><b>Hobbies:</b> Cooking and Organizing Activities.</p>
+      <p><b>Career Goal:</b> To secure a good position where I can use my skills, learn new things, grow in my career.</p>
+    </div>
+  )
+}
